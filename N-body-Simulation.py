aa27f9201e695f74e7ecd1b01825fa369b12f040
Ajay Ramanathan <ajay.010293@gmail.com>
2014-03-16 09:57:45 +0530

    Intial Commit

---
+++ N-body-Simulation.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python 
+# A N-Body problem solver 
+
+from numpy import *
+from random import random,seed
+import Tkinter
+from PIL import Image, ImageDraw
+import ImageTk
+from time import time
