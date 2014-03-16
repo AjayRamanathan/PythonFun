75acaa037dd7277a53c69a254a8c1486c8553ff5
Ajay Ramanathan <ajay.010293@gmail.com>
2014-03-16 10:53:46 +0530

    Random Mass,Position and Velocity

--- N-body-Simulation.py
+++ N-body-Simulation.py
@@ -2,8 +2,13 @@
 # A N-Body problem solver 
 
 from numpy import *
-from random import random,seed
-import Tkinter
-from PIL import Image, ImageDraw
-import ImageTk
-from time import time
+
+N = 100
+
+MASS = zeros(N,dtype=float)
+POS = zeros((N,2),dtype=float)
+VEL = zeros((N,2),dtype=float)
+
+print MASS 
+print POS 
+print VEL
