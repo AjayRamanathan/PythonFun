9d0f7477425c0fd4d642118ccd28daeefaa33918
Ajay Ramanathan <ajay.010293@gmail.com>
2014-03-16 12:20:12 +0530

    Created Particle Class

--- N-body-Simulation.py
+++ N-body-Simulation.py
@@ -1,13 +1,25 @@
 #! /usr/bin/python 
 # A N-Body problem solver 
+#
 
 from numpy import *
+from random import random,seed
 
 N = 100
 
-MASS = zeros(N,dtype=float)
-POSTION = zeros((N,2),dtype=float)
-VELOCITY = zeros((N,2),dtype=float)
+class Objects:
+    def __init__(self, mass, position, velocity, acceleration, radius):
+        self.mass = mass
+        self.position = position
+        self.velocity = velocity
+        self.acceleration = acceleration
+        self.radius = radius
+
+    def __repr__(self):
+       return self.__str__()
+
+    def __str__(self):
+       return "<<%g,%g,%g,%g>>" % (self.mass,self.position,self.velocity,self.acceleration)
 
 class Universe:
     def __init__(self,edge,dimension=2):
@@ -15,7 +27,7 @@ class Universe:
         self.edge = array(edge,dtype=float)
         self.dimension = dimension
         self.center = array( [(self.edge[2]+self.edge[0])/2, (self.edge[3]+self.edge[1])/2] , dtype=float)
-        self.sideLength = self.max() - self.min()
+        self.length = self.max() - self.min()
 
     def max(self):
         return self.edge[self.dimension:]
@@ -29,9 +41,17 @@ class Universe:
     def __str__(self):
         return "<<%g,%g,%g,%g>>" % (self.edge[0],self.edge[1],self.edge[2],self.edge[3])
 
-UNIVERSE = Universe([0,0,10,10])        
+UNIVERSE = Universe([-10,-10,10,10])  
+OBJECTS = []
+for i in xrange(N):
+    MASS = 1
+    POSITION = UNIVERSE.min() + array([random(),random()])*UNIVERSE.length
+    VELOCITY = UNIVERSE.min() + array([random(),random()])*UNIVERSE.length
+    ACCELERATION = UNIVERSE.min() + array([random(),random()])*UNIVERSE.length
+    RADIUS = 1
+    OBJECTS = Objects(MASS, POSITION, VELOCITY, ACCELERATION, RADIUS)
+    
 
-print UNIVERSE.sideLength
-print UNIVERSE.center
-print UNIVERSE.edge
-print UNIVERSE.dimension
+
+
+
