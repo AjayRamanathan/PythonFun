07ae3a3d8afa81c886a065dea4dc0e5437364f29
Ajay Ramanathan <ajay.010293@gmail.com>
2014-03-17 03:08:52 +0530

    system; exagerated radius

--- Sun-Jupiter.py
+++ Sun-Jupiter.py
@@ -5,45 +5,54 @@ import math
 
 
 def main():
+	N = 3*(365*10+2)
+	print N
 	clock = pygame.time.Clock()
-	R = 100
+	R = 200
+	constant = 2000000000/(800*R)
 	pygame.init()
 	screen=pygame.display.set_mode((800,800))
 	pygame.display.set_caption('Basic Pygame program')
-	constant = 2000000000/(800*R)
-	print constant
-	sun_radius = 696342/constant
-	print sun_radius
+
 	sun_x,sun_y = 400,400
 	sun_color = (250, 250, 0)
+	sun_radius = 696342/constant
 
-	color = (0, 0, 0)
-	radius = 69911/constant
+	jb_x,jb_y = 200,200
+	jb_color = (250, 100, 0)
+	jb_radius = 69911/constant
+
+	cg_color = (0, 0, 0)
+	cg_radius = 4*10000/constant
 
 	background = pygame.Surface(screen.get_size())
 	background = background.convert()
 	background.fill((250, 250, 250))
-	
-	font = pygame.font.Font(None, 36)
-	text = font.render("Hello There", 1, (10, 10, 10))
-	textpos = text.get_rect()
-	textpos.centerx = background.get_rect().centerx
-	background.blit(text, textpos)
 
 	screen.blit(background, (0, 0))
 	pygame.display.flip()
 
+	n = 0
 	while 1:
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				return
-
-		screen.blit(background, (0, 0))
-		x,y=pygame.mouse.get_pos()
-		pygame.draw.circle(screen, sun_color, (sun_x, sun_y), int(sun_radius+1))
-		pygame.draw.circle(screen, color, (x, y), radius)		
-		pygame.display.flip()
+		if n > N:
+			font = pygame.font.Font(None, 36)
+			text = font.render("End of event", 1, (10, 10, 10))
+			textpos = text.get_rect()
+			textpos.centerx = background.get_rect().centerx
+			textpos.centery = background.get_rect().centery
+			background.blit(text, textpos)
+		else:
+			screen.blit(background, (0, 0))
+			x,y=pygame.mouse.get_pos()
+			pygame.draw.circle(screen, sun_color, (sun_x, sun_y), int(sun_radius+1))
+			pygame.draw.circle(screen, cg_color, (x, y), cg_radius)
+			pygame.draw.circle(screen, jb_color, (jb_x, jb_y), jb_radius)		
+			pygame.display.flip()
 		
+		n += 1
 	clock.tick(30)
 
 if __name__ == '__main__': main()	
