6291dedb1c12319950a430ef1fcfa0f41db60486
Ajay Ramanathan <ajay.010293@gmail.com>
2014-03-16 12:59:40 +0530

    Created Class Allobjects

--- N-body-Simulation.py
+++ N-body-Simulation.py
@@ -5,21 +5,32 @@
 from numpy import *
 from random import random,seed
 
+G= 0.125
 N = 100
 
 class Objects:
-    def __init__(self, mass, position, velocity, acceleration, radius):
+    def __init__(self, mass, position, velocity, acceleration):
         self.mass = mass
         self.position = position
         self.velocity = velocity
         self.acceleration = acceleration
-        self.radius = radius
-
+        self.radius = math.sqrt(self.mass)
+    
+    def __iter__(self):
+        return iter(self.position)
+    
     def __repr__(self):
        return self.__str__()
 
     def __str__(self):
        return "<<%g,%g,%g,%g>>" % (self.mass,self.position,self.velocity,self.acceleration)
+
+class Allobjects:
+     def __init__(self):
+        self.objects = []
+
+     def add_object(self, mass, position, velocity, acceleration):
+        self.objects.append(Objectsjects(mass, position, velocity, acceleration))
 
 class Universe:
     def __init__(self,edge,dimension=2):
@@ -42,16 +53,44 @@ class Universe:
         return "<<%g,%g,%g,%g>>" % (self.edge[0],self.edge[1],self.edge[2],self.edge[3])
 
 UNIVERSE = Universe([-10,-10,10,10])  
-OBJECTS = []
+
+OBJECTS = Allobjects
+
 for i in xrange(N):
     MASS = 1
     POSITION = UNIVERSE.min() + array([random(),random()])*UNIVERSE.length
     VELOCITY = UNIVERSE.min() + array([random(),random()])*UNIVERSE.length
     ACCELERATION = UNIVERSE.min() + array([random(),random()])*UNIVERSE.length
-    RADIUS = 1
-    OBJECTS = Objects(MASS, POSITION, VELOCITY, ACCELERATION, RADIUS)
-    
+    OBJECTS.add_object(MASS, POSITION, VELOCITY, ACCELERATION)
 
+def getForce(i1, i2):
+
+    vector = i2.position-i1.position
+    radius = sqrt(vector.dot(vector))
+    force = array( vector * G*p.mass*p2.mass / radius**3 )
+    return force 
+
+for i1 in OBJECTS.Objects:
+    for i2 in OBJECTS.Objects:
+        if i1 != i2:
+            acceleration = getForce(i1, i2) / i1.mass
+            i1.acceleration += acceleration
+
+for i in ALLOBJECTS.Objects:
+	i.velocity += i.acceleration
+        i.position += i.velocity
+
+for i1 in Particles:
+    for i2 in Particles:
+        if i1 != i2:
+            vector = i2.position-i1.position
+            radius = sqrt(vector.dot(vector))
+            if Distance < (i1.radius+i2.radius):
+                p.velocity = ((i.mass*i.velocity)+(i2.mass*i2.velocity))/(i.mass+i2.mass)
+                i.position = ((i.mass*i.position)+(i2.mass*i2.position))/(i.mass+i2.mass)
+                i.mass += i2.mass
+                i.radius = math.sqrt(i.mass)
+                Objects.remove(i2)
 
 
 
