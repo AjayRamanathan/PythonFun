8dc2d9d7c7af42879ee82b6d2fd1bc8efcd34b1f
Ajay Ramanathan <ajay.010293@gmail.com>
2014-03-17 03:39:54 +0530

    juptier speed and position

--- Sun-Jupiter.py
+++ Sun-Jupiter.py
@@ -1,15 +1,24 @@
 import pygame, sys
 from pygame.locals import*
 from sys import exit
+from numpy import *
 import math
+
 
 
 def main():
 	N = 3*(365*10+2)
-	print N
-	clock = pygame.time.Clock()
 	R = 200
 	constant = 2000000000/(800*R)
+
+	JUPITER = zeros((N,6), dtype=float)
+	Jupiter_a = (5.096210785220794E+08, -5.625673003639891E+08, -9.109215289602894E+06,  9.531092644846060E+00,  9.401837023552590E+00, -2.522073348918472E-01)
+	Jupiter_b = (-1.986100514188347E+08, -7.697020902092769E+08,  7.614122074352508E+06,  1.250778592291153E+01, -2.651322314721329E+00, -2.693549489780306E-01)
+	Jupiter_c = (7.496501615105320E+08, -3.201712734926724E+08,  1.811158327190959E+07,  4.981904715194972E+00, -1.141748300836142E+01, -6.466624164173854E-02)
+	JUPITER[1] = Jupiter_a
+	print JUPITER[1]
+
+	clock = pygame.time.Clock()
 	pygame.init()
 	screen=pygame.display.set_mode((800,800))
 	pygame.display.set_caption('Basic Pygame program')
@@ -48,6 +57,7 @@ def main():
 			pygame.display.flip()
 		else:
 			screen.blit(background, (0, 0))
+			jb_x,jb_y = JUPITER[1][1], JUPITER[1][2]
 			x,y=pygame.mouse.get_pos()
 			pygame.draw.circle(screen, sun_color, (sun_x, sun_y), int(sun_radius+1))
 			pygame.draw.circle(screen, cg_color, (x, y), cg_radius)
