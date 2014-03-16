7d363b8679248578428768bd1fb61ab5e6420d81
Ajay Ramanathan <ajay.010293@gmail.com>
2014-03-16 15:40:49 +0530

    No Collision N-body system

--- N-body-Simulation.py
+++ N-body-Simulation.py
@@ -8,7 +8,7 @@ from PIL import Image, ImageDraw
 import ImageTk
 from time import time
 
-N = 100
+N = 2
 G = 0.125
 
 MASS = zeros(N,dtype=float)
@@ -24,100 +24,100 @@ GRID_SIZE = 10
 CIRCLE_RADIUS = 10
 
 class Universe:
-    def __init__(self,edge,dimension=2):
-        assert(dimension*2 == len(edge))
-        self.edge = array(edge,dtype=float)
-        self.dimension = dimension
-        self.center = array( [(self.edge[2]+self.edge[0])/2, (self.edge[3]+self.edge[1])/2] , dtype=float)
-        self.length = self.max() - self.min()
+	def __init__(self,edge,dimension=2):
+		assert(dimension*2 == len(edge))
+		self.edge = array(edge,dtype=float)
+		self.dimension = dimension
+		self.center = array( [(self.edge[2]+self.edge[0])/2, (self.edge[3]+self.edge[1])/2] , dtype=float)
+		self.length = self.max() - self.min()
 
-    def max(self):
-        return self.edge[self.dimension:]
-    
-    def min(self):
-        return self.edge[:self.dimension]
-    
-    def __repr__(self):
-        return self.__str__()
+	def max(self):
+		return self.edge[self.dimension:]
+	
+	def min(self):
+		return self.edge[:self.dimension]
+	
+	def __repr__(self):
+		return self.__str__()
 
-    def inside(self,p):
-        if any(p < self.min()) or any(p > self.max()):
-            return False
-        else:
-            return True    
-    
-    def __str__(self):
-        return "<<%g,%g,%g,%g>>" % (self.edge[0],self.edge[1],self.edge[2],self.edge[3])
+	def inside(self,p):
+		if any(p < self.min()) or any(p > self.max()):
+			return False
+		else:
+			return True    
+	
+	def __str__(self):
+		return "<<%g,%g,%g,%g>>" % (self.edge[0],self.edge[1],self.edge[2],self.edge[3])
 
 UNIVERSE = Universe([0,0,10,10])        
 
 for i in xrange(N):
-    MASS[i] = 1
-    POSITION[i] = UNIVERSE.min() + array([random(),random()])*UNIVERSE.length
+	MASS[i] = 1
+	POSITION[i] = UNIVERSE.min() + array([random(),random()])*UNIVERSE.length
 
 class Physics:
-    def __init__(self, universe, N, position, velocity,acceleration, mass):
-        self.universe = universe
-        self.N = N
-        self.mass = mass
-        self.position = position
-        self.velocity = velocity
-        self.acceleration = acceleration
+	global N, POSITION, VELOCITY, ACCELERATION, MASS
+	def __init__(self, universe):
+		self.universe = universe
 
-    def updateSys(self, Skip_Time):
-        self.calculateAcceleration()
-        self.velocity += self.acceleration * Skip_Time
-        self.position += self.velocity * Skip_Time  + self.acceleration * Skip_Time * Skip_Time
+	def updateSys(self, Skip_Time):
+		global VELOCITY, POSITION
+		self.calculateAcceleration()
+		VELOCITY = VELOCITY + ACCELERATION * Skip_Time
+		POSITION += VELOCITY * Skip_Time  + ACCELERATION * Skip_Time * Skip_Time
 
-    def calculateAcceleration(self):
-        for i in xrange(self.N):
-            self.acceleration[i] = self.calculateBodyAcceleration(i)
+	def calculateAcceleration(self):
+		for i in xrange(N):
+			ACCELERATION[i] = self.calculateBodyAcceleration(i)
 
-    def calculateBodyAcceleration(self, k):              
-        acc = zeros((1,2),dtype=float)
-        for i in xrange(self.N):
-        	if k != i:
-        	   acc += getAcceleration(self.position[k], self.mass[k], self.position[i], self.mass[i])
+	def calculateBodyAcceleration(self, k):              
+		acc = zeros((1,2),dtype=float)
+		for i in xrange(N):
+			if k != i:
+			   acc += getAcceleration(POSITION[k], MASS[k], POSITION[i], MASS[i])
+		return acc
 
 def getAcceleration(p1,m1,p2,m2):
-    vector = p2-p1
-    radius = sqrt(vector.dot(vector))
-    acceleration = array(( vector * G*m1*m2 / radius**3 ))/m1  
-    return acceleration    	   
-    
-sys = Physics(UNIVERSE, N, POSITION, VELOCITY, ACCELERATION, MASS)
+	vector = p2-p1
+	radius = sqrt(vector.dot(vector))
+	acceleration = array(( vector * G*m1*m2 / radius**3 ))/m1  
+	return acceleration    	   
+	
+sys = Physics(UNIVERSE)
 
 def drawBodies(drawPtr):
-    for x in xrange(N):
-        if UNIVERSE.inside(POSITION[x]):
-            p = convertPos(POSITION[x])
-            r = MASS[x]*5
-            drawPtr.ellipse( join(p-r,p+r) , fill = (0,0,0) )
+	for x in xrange(N):
+		if UNIVERSE.inside(POSITION[x]):
+			p = convertPos(POSITION[x])
+			r = MASS[x]*5
+			drawPtr.ellipse( join(p-r,p+r) , fill = (0,0,0) )
 
 def convertPos(p):
-    c = trunc( ( p - UNIVERSE.min()) / (UNIVERSE.max()-UNIVERSE.min()) * array(IMAGE_SIZE) ).tolist()
-    c[1] = IMAGE_SIZE[1] - c[1]
-    return c
+	c = trunc( ( p - UNIVERSE.min()) / (UNIVERSE.max()-UNIVERSE.min()) * array(IMAGE_SIZE) ).tolist()
+	c[1] = IMAGE_SIZE[1] - c[1]
+	return c
+
+def join(p1,p2):
+    return ([p1[0],p1[1],p2[0],p2[1]])	
 
 def task_update():
-    global Time,boo
-    Time += Skip_Time
+	global Time,boo
+	
+	Time += Skip_Time
+	im = Image.new('RGB', IMAGE_SIZE, (255,255,255))
+	
+	draw = ImageDraw.Draw(im)
+	drawBodies(draw)
+	sys.updateSys(Skip_Time)
 
-    im = Image.new('RGB', IMAGE_SIZE, (255,255,255))
-    draw = ImageDraw.Draw(im)
+	boo = ImageTk.PhotoImage(im)
+	label_image.config(image=boo)
+	label_image.pack()
 
-    drawBodies(draw)
-
-    sys.updateSys(Skip_Time)
-
-    boo = ImageTk.PhotoImage(im)
-    label_image.config(image=boo)
-    label_image.pack()
-
-    root.after(10,task_update)
+	root.after(10,task_update)
 
 def button_click_exit_mainloop (event):
-    event.widget.quit() 
+	event.widget.quit() 
 
 root = Tkinter.Tk()
 root.bind("<Button>", button_click_exit_mainloop)
